6b1af76f44a40b4eda23c3892c7312923b92b71a
knxie411 <hyunjun20000411@gmail.com>
2022-04-10 21:39:41 +0900

    YaMae... 난 가망이 없따...

---
+++ Lecture05/hanoi.js
@@ -0,0 +1,12 @@
+var F = document.getElementsByClassName("First");
+var S = document.getElementsByClassName("Second");
+var T = document.getElementsByClassName("Third");
+
+
+
+
+function Init()
+{
+    F[0].innerHTML = "🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥<br>🟨🟨🟨🟨🟨🟨🟨🟨🟨<br>🟩🟩🟩🟩🟩🟩🟩<br>🟦🟦🟦🟦🟦<br>🟪🟪🟪<br>⬛️";   
+
+}
